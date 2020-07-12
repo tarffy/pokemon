97026aeebcc97761454dedca76184c3715b7619a
whx123abc <42049491+whx123abc@users.noreply.github.com>
2020-07-12 15:52:24 +0800

    基本完成了client socket连接准备

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -4,4 +4,39 @@ Client::Client(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+	socket = new QTcpSocket(this);
+	socket->connectToHost(QHostAddress::LocalHost, 8888);
+	if (socket->waitForConnected()) {
+		ui.Text_status->setText("connected");
+	}
+	else {
+		ui.Text_status->setText("connect failed");
+	}
+	connect(ui.Button_socket, &QPushButton::clicked, this, &Client::send_to_socket);
+	connect(socket, &QTcpSocket::readyRead, this, &Client::read_from_socket);
+	connect(ui.Button1, &QPushButton::clicked,  this, &Client::change_to_battle);
+	connect(ui.Button2, &QPushButton::clicked, this, &Client::back_to_menu);
+}
+
+void Client::change_to_battle()
+{
+	ui.Swidgt->setCurrentIndex(1);
+}
+
+void Client::back_to_menu()
+{
+	ui.Swidgt->setCurrentIndex(0);
+}
+
+void Client::send_to_socket()
+{
+	QString str = ui.Text_send->toPlainText();
+	socket->write(str.toLocal8Bit());
+}
+
+void Client::read_from_socket()
+{
+	QString str = socket->readAll();
+	ui.Text_read->append(str);
 }
--- Client/Client.h
+++ Client/Client.h
@@ -2,7 +2,8 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_Client.h"
-
+#include <qpushbutton.h>
+#include <QtNetwork>
 class Client : public QMainWindow
 {
     Q_OBJECT
@@ -12,4 +13,10 @@ public:
 
 private:
     Ui::ClientClass ui;
+	QTcpSocket *socket;
+public:
+	void change_to_battle();
+	void back_to_menu();
+	void send_to_socket();
+	void read_from_socket();
 };
